f108ba251c6f1f2a8dcc0e150ca9d423e6d2824a
hassanqari9 <you@example.comhassanqari9@gmail.com>
2022-09-25 19:28:03 +0530

    da

--- attendance.js
+++ attendance.js
@@ -1,206 +1,167 @@
-class attendance
-{
-// private:
-    
-// public:
-    constructor(c, a, b) // constructor
-    {
+class attendance {
+    constructor(
+        c,
+        a,
+        b // constructor
+    ) {
         this.Criteria = c / 100;
         this.Classes_attended = a;
         this.Total_classes_held = b;
-        // set_Criteria();
-        // set_Attended();
-        // set_Total_classes_held();
-
     }
 
-    // mutators
-    // set_Attended()
-    // {
-    //     Classes_attended = this.a;
-    // }
-
-    // set_Total_classes_held()
-    // {
-    //     Total_classes_held = this.b;
-    // }
-
-    // set_Criteria()
-    // {
-    //     Criteria = this.c / 100;
-    // }
-
-    // accessors
-    // get_Attended()
-    // {
-    //     return Classes_attended;
-    // }
-
-    // get_Total_classes_held()
-    // {
-    //     return Total_classes_held;
-    // }
-
-    // get_Criteria()
-    // {
-    //     return Criteria;
-    // }
-
-    get_Attendance_ratio()
-    {
-        var Attendance_ratio = (this.Classes_attended / this.Total_classes_held);
+    get_Attendance_ratio() {
+        var Attendance_ratio = this.Classes_attended / this.Total_classes_held;
         return Attendance_ratio;
     }
 
-    get_Attendance_percentage()
-    {
-
-        if (a1.Argument_verifier() == 1)
-        {
-            exit;// The program would terminate if invalid arguments are inputted
-        }
-        else
-        {
-            document.querySelector(".text3").innerHTML="Current attendance: "+(a1.get_Attendance_ratio() * 100)+"%";
-            // cout << "Current attendance: " << (get_Attendance_ratio() * 100) << "%" << endl;
-            
+    get_Attendance_percentage() {
+        if (a1.Argument_verifier() == 1) {
+            exit; // The program would terminate if invalid arguments are inputted
+        } else {
+            document.querySelector(".text3").innerHTML =
+                "Current attendance: " + a1.get_Attendance_ratio() * 100 + "%";
         }
     }
 
     // Verification of Incorrect Arguments inputted by a user
-    Argument_verifier()
-    {
-        if (this.Criteria <= 0 || this.Classes_attended < 0 || this.Total_classes_held <= 0 || this.Classes_attended > this.Total_classes_held)
-        {
-            document.querySelector(".text2").innerHTML=" ";
-            document.querySelector(".text3").innerHTML="Incorrect argument(s).";
-            // cout << "Incorrect argument(s)." << endl;
+    Argument_verifier() {
+        if (
+            this.Criteria <= 0 ||
+            this.Classes_attended < 0 ||
+            this.Total_classes_held <= 0 ||
+            this.Classes_attended > this.Total_classes_held
+        ) {
+            document.querySelector(".text2").innerHTML = " ";
+            document.querySelector(".text3").innerHTML = "Incorrect argument(s).";
             return 1;
-            
-        }
-        else
-        {
+        } else {
             return 2;
         }
-            
     }
 
     // selects whether a user wants to know the shortage or the excess classes that may be skipped
-    selector()
-    {
-        if (a1.get_Attendance_ratio() < this.Criteria)
-            a1.shortage();
-
-        else
-            a1.excess();
+    selector() {
+        if (a1.get_Attendance_ratio() < this.Criteria) a1.shortage();
+        else a1.excess();
     }
 
-    shortage()
-    {
-        var trigger = 0, i = 0;
-        var variable_attendance_ratio = 0, m, n;
+    shortage() {
+        var trigger = 0,
+            i = 0;
+        var variable_attendance_ratio = 0,
+            m,
+            n;
         m = this.Classes_attended;
         n = this.Total_classes_held;
 
-        for (var i = 0; i < 36500; i++)
-        {
+        for (var i = 0; i < 36500; i++) {
             variable_attendance_ratio = m / n;
             m++;
             n++;
 
-            if (variable_attendance_ratio >= this.Criteria)
-            {
-                switch (i) // To fix grammatical errors. Case 1 is for Sinlugar & Case 2 is for Plural.
-                {
-                case 1: // Test Case(rare case): 75/7.4/10 (could not find whole numbers)
-                    document.querySelector(".text2").innerHTML="You need to attend " + i + " more class to get attendance of " +(i + this.Classes_attended) / (i + this.Total_classes_held) * 100+ "%.";
-                    trigger = 10;
-                    break;
+            if (variable_attendance_ratio >= this.Criteria) {
+                switch (
+                i // To fix grammatical errors. Case 1 is for Sinlugar & Case 2 is for Plural.
+                ) {
+                    case 1: // Test Case(rare case): 75/7.4/10 (could not find whole numbers)
+                        document.querySelector(".text2").innerHTML =
+                            "You need to attend " +
+                            i +
+                            " more class to get attendance of " +
+                            ((i + this.Classes_attended) / (i + this.Total_classes_held)) *
+                            100 +
+                            "%.";
+                        trigger = 10;
+                        break;
 
-                default: // Test Case(normal case): 75/5/10
-                    document.querySelector(".text2").innerHTML="You need to attend " + i + " more classes to get attendance above " + this.Criteria * 100 + "%, after which your attendace will be: " +(i + this.Classes_attended) / (i + this.Total_classes_held) * 100+ "%.";
-                    trigger = 10;
-                    break;
+                    default: // Test Case(normal case): 75/5/10
+                        document.querySelector(".text2").innerHTML =
+                            "You need to attend " +
+                            i +
+                            " more classes to get attendance above " +
+                            this.Criteria * 100 +
+                            "%, after which your attendace will be: " +
+                            ((i + this.Classes_attended) / (i + this.Total_classes_held)) *
+                            100 +
+                            "%.";
+                        trigger = 10;
+                        break;
                 }
                 break;
             }
         }
-        if (trigger == 0)
-        {
-            document.querySelector(".text2").innerHTML="The attendance percentage could not reach " + this.Criteria * 100 + "% in next 36500 days."; // This is never occur unless someone is messing around. Test Case: 75/1/30,000
+        if (trigger == 0) {
+            document.querySelector(".text2").innerHTML =
+                "The attendance percentage could not reach " +
+                this.Criteria * 100 +
+                "% in next 36500 days."; // This is never occur unless someone is messing around. Test Case: 75/1/30,000
         }
     }
 
-    excess()
-    {
+    excess() {
         var i = 0;
-        var variable_attendance_ratio = 0, m, n;
+        var variable_attendance_ratio = 0,
+            m,
+            n;
         m = this.Classes_attended;
         n = this.Total_classes_held;
 
-        for (var i = 0; i < 36500; i++)
-        {
+        for (var i = 0; i < 36500; i++) {
             variable_attendance_ratio = m / n;
             n++;
 
-            if (variable_attendance_ratio < this.Criteria)
-            {
-                if ((i - 1) == 0) // has more than required attendace but skipping even one class would result in shortage.
-                {
-                    document.querySelector(".text2").innerHTML="You may skip no classes. If you skip a class your attendance will be: " +(this.Classes_attended / (this.Total_classes_held + 1)) * 100+ "%."; // Test Case: 75/8/10
-                }
-                else
-                {
-                    document.querySelector(".text2").innerHTML="You can skip "+(i - 1)+" classes. After skipping your attendance will be: " + (this.Classes_attended / (this.Total_classes_held + (i - 1))) * 100+"%"; // Test Case(normal case): 75/10/11
+            if (variable_attendance_ratio < this.Criteria) {
+                if (i - 1 == 0) {
+                    // has more than required attendace but skipping even one class would result in shortage.
+                    document.querySelector(".text2").innerHTML =
+                        "You may skip no classes. If you skip a class your attendance will be: " +
+                        (this.Classes_attended / (this.Total_classes_held + 1)) * 100 +
+                        "%."; // Test Case: 75/8/10
+                } else {
+                    document.querySelector(".text2").innerHTML =
+                        "You can skip " +
+                        (i - 1) +
+                        " classes. After skipping your attendance will be: " +
+                        (this.Classes_attended / (this.Total_classes_held + (i - 1))) *
+                        100 +
+                        "%"; // Test Case(normal case): 75/10/11
                 }
                 break;
-            }
-
-            else if (variable_attendance_ratio == this.Criteria)
-            {
-                
-                if (i == 0) // when no classes can be skipped as attendace is equal to criteria
-                {
-                    document.querySelector(".text2").innerHTML="Your attendace is exactly " + this.Criteria * 100 + "% and hence you may skip no classes. If you skip a class your attendance will be: " +(this.Classes_attended / (this.Total_classes_held + 1)) * 100+ "%."; // Test Case: 75/6/8
-                }
-                else // when the loop eventually reaches value exactly equal to criteria
-                {
-                    document.querySelector(".text2").innerHTML="After skipping " + i + " classes your attendance will be " + this.Criteria * 100 + "%."; // Test Case: 75/6/7
+            } else if (variable_attendance_ratio == this.Criteria) {
+                if (i == 0) {
+                    // when no classes can be skipped as attendace is equal to criteria
+                    document.querySelector(".text2").innerHTML =
+                        "Your attendace is exactly " +
+                        this.Criteria * 100 +
+                        "% and hence you may skip no classes. If you skip a class your attendance will be: " +
+                        (this.Classes_attended / (this.Total_classes_held + 1)) * 100 +
+                        "%."; // Test Case: 75/6/8
+                } // when the loop eventually reaches value exactly equal to criteria
+                else {
+                    document.querySelector(".text2").innerHTML =
+                        "After skipping " +
+                        i +
+                        " classes your attendance will be " +
+                        this.Criteria * 100 +
+                        "%."; // Test Case: 75/6/7
                 }
                 break;
             }
         }
     }
-};
+}
 
-$("button").click(function(){
+$("button").click(function () {
     var value_3 = document.getElementById("criteria").value;
     var value_1 = document.getElementById("classes").value;
     var value_2 = document.getElementById("total").value;
 
-    a1 = new attendance(parseFloat(value_3), parseFloat(value_1), parseFloat(value_2));
+    a1 = new attendance(
+        parseFloat(value_3),
+        parseFloat(value_1),
+        parseFloat(value_2)
+    );
 
     a1.get_Attendance_percentage();
     a1.selector();
-    
 });
-
-// int main()
-// {
-    // double value_1, value_2, value_3;
-
-    // cout << "Enter the attendace percentage criteria set by your institution: ";
-    // cin >> value_3;
-
-    // cout << "Enter the number of days attended: ";
-    // cin >> value_1;
-
-    // cout << "Enter total number of classes: ";
-    // cin >> value_2;
-    
-
-    // attendance a1(value_3, value_1, value_2);
-
-    // a1.get_Attendance_percentage();
-    // a1.selector();
-// }
